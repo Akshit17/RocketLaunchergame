77b08cb278e40eeec16718e751b480f68ffe6ed3
Akshit17 <69861063+Akshit17@users.noreply.github.com>
2020-12-06 16:47:51 +0530

    Add files via upload

---
+++ Assets/CameraFollowUp.cs
@@ -0,0 +1,51 @@
+﻿
+
+using UnityEngine;
+
+public class CameraFollowUp : MonoBehaviour
+{
+    public Transform target ;
+   public float smoothspeed = 10f;
+     public Vector3 offset ;
+    
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(desiredPosition, target.position, smoothspeed * Time.deltaTime);
+
+
+        transform.position = smoothedPosition;
+   
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/RocketLAUNCH.cs
@@ -0,0 +1,170 @@
+﻿
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RocketLAUNCH : MonoBehaviour
+{
+   
+    
+    
+    [SerializeField] float mainThrust = 650f;
+    [SerializeField] float rotateThrust = 50f;
+    new Rigidbody rigidbody;
+    AudioSource thrustaudio;
+
+    enum State { Alive , Dying , Ascending};
+    State state;
+   
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rigidbody = GameObject.Find("Rocket").GetComponent<Rigidbody>();
+        thrustaudio = GetComponent<AudioSource>();
+        state = State.Alive ; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float rotateThrustframe = rotateThrust * Time.deltaTime;
+
+
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+         
+    }
+
+    void Thrust()
+    {
+        float mainThrustframe = mainThrust * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+          
+            rigidbody.AddRelativeForce(Vector3.up * mainThrustframe);
+
+
+          
+
+
+         if (!thrustaudio.isPlaying)
+            {
+            
+                thrustaudio.Play();
+          
+            }
+
+
+        }
+
+        else
+        {
+           Invoke("StoppingSound" , 0.05f);
+
+        }
+    }
+
+    private void StoppingSound()
+    {
+        thrustaudio.Stop();
+    }
+
+    void Rotate()
+    {
+
+        float rotateThrustframe = rotateThrust * Time.deltaTime;
+       
+        rigidbody.freezeRotation = true;
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotateThrustframe);
+
+        }
+
+
+
+
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(-Vector3.forward * rotateThrustframe);
+
+
+        }
+
+        rigidbody.freezeRotation = false;
+
+    }
+
+
+
+  void OnCollisionEnter(Collision collision)
+    {
+
+        switch (collision.gameObject.tag)
+        {
+
+            case "Friendly":
+                {
+                    
+                        break;
+                        }
+
+            case "Endpoint":
+                {
+                    state = State.Ascending;
+                   Invoke("NextLevelLoad", 0.5f);
+                    break;
+                }
+
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 0.5f);
+                break;
+
+        };
+
+
+
+      
+             
+     
+        }
+
+   void NextLevelLoad()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+     void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
+
+
+
+
+    
+
+
+
+        
+
+
+
+
+
